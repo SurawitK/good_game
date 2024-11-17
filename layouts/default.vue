9c7008e5c5dc4eb5f1d3a3a68c3078f1ec4494f7
SurawitK <sk.helloworld@gmail.com>
2024-11-17 22:44:59 +0700

    NavV0.0.1

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,30 @@
+<script setup lang="ts">
+const links = [
+	{
+		label: "Good Game",
+		to: "/",
+	},
+
+	{ label: "Sign Up", to: "/" },
+	{ label: "Login", to: "/" },
+];
+</script>
+
+<!-- <UIcon name="i-heroicons-light-bulb" class="w-5 h-5" /> -->
+
 <template>
-  <UContainer>
-    <h1 class="text-8xl text-red-500 border border-red-400 border-4">Good Game</h1>
-    <Placeholder class="h-32 bg-green-10" />
-  </UContainer>
-    <slot />
-  </template>
+	<header>
+		<div>
+      <UHorizontalNavigation
+        :links="links"
+        class="border-b border-gray-200 dark:border-gray-800">
+      </UHorizontalNavigation>
+    </div>
+		<UIcon
+			name="i-lucide-globe"
+			class="w-5 h-5" />
+
+		<!-- <Placeholder class="h-32 bg-green-10" /> -->
+	</header>
+	<slot />
+</template>
