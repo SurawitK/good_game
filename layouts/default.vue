253b0d348ca381b2256b2023df7d6c9de485f957
SurawitK <sk.helloworld@gmail.com>
2024-11-17 21:12:23 +0700

    NavV0.0.1

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,7 @@
 <template>
-    <div>
-      <p>Some default layout content shared across all pages</p>
-      <slot />
-    </div>
+  <UContainer>
+    <h1 class="text-8xl text-red-500 border border-red-400 border-4">Good Game</h1>
+    <Placeholder class="h-32 bg-green-10" />
+  </UContainer>
+    <slot />
   </template>
