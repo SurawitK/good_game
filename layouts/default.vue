3f6834db7a698787eb8879411840e0067b496f09
possionthefish1 <92135698+possionthefish1@users.noreply.github.com>
2024-12-04 21:55:17 +0700

    b4 give muak 2 commit

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,34 +2,10 @@
 	const languages = [
 		[{ label: 'English' }, { label: 'ภาษาไทย' }],
 	];
-	/* 	const links = [
-		{
-			label: "Good Game",
-			to: useLink({ to: "/" }),
-		},
-
-		{ label: "Sign Up", to: useLink({ to: "/signup" }) },
-		{ label: "Login", to: useLink({ to: "/login" }) },
-	];
-	const items = ref([
-		{ label: "Good Game", to: useLink({ to: "/" }) },
-		{ label: "Sign Up", to: useLink({ to: "/signup" }) },
-		{ label: "Login", to: useLink({ to: "/login" }) },
-	]); */
 </script>
-
-<!-- <UIcon name="i-heroicons-light-bulb" class="w-5 h-5" /> -->
 
 <template>
 	<header>
-		<!-- <div>
-			<UHorizontalNavigation
-				:links="links"
-				class="border-b border-gray-200 dark:border-gray-800"></UHorizontalNavigation>
-		</div>
-		<UIcon
-			name="i-lucide-globe"
-			class="w-5 h-5" /> -->
 		<nav class="flex justify-between gap-4 p-6">
 			<div class="flex gap-4">
 				<NuxtLink
@@ -69,20 +45,6 @@
 						color="white"
 						variant="ghost" />
 				</UDropdown>
-				<!-- <UIcon
-					@click="showSelect = !showSelect"
-					v-motion
-					:initial="{ scale: 1 }"
-					:hovered="{ scale: 1.05 }"
-					:tapped="{ scale: 0.95 }"
-					:focused="{ scale: 1.05 }"
-					name="i-lucide-globe"
-					class="w-5 h-5 hover:opacity-50 cursor-pointer"
-					tabindex="1" /> -->
-
-				<!-- <div class="relative">
-					<p class="fixed absolute top-0 bottom-0"></p>
-				</div> -->
 
 				<UButton
 					v-motion
@@ -101,8 +63,6 @@
 				</UButton>
 			</div>
 		</nav>
-
-		<!-- <Placeholder class="h-32 bg-green-10" /> -->
 	</header>
 	<slot />
 </template>
